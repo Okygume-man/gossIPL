4ae6ab5d4b7270c30020e51eae9dce393e708049
Maxence POINTUD <maxence.pointud@gmail.com>
2019-08-09 15:32:49 +0200

    First Commit

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,23 @@
+class GossipsController < ApplicationController
+  def index
+    @gossip = Gossip.new
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
